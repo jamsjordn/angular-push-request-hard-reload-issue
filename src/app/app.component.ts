529139985b12ffcf752b7449315e7a930c8e861f
James Jordan <james@clipboard.app>
2023-05-31 15:58:35 +1000

    add service worker and swPush.requestSubscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html', 
 })
 export class AppComponent {
   title = 'base-angular';
+
+  constructor(private readonly swPush: SwPush) {}
+
+  public registerForNotifications(): void {
+    this.swPush.requestSubscription({
+      serverPublicKey:
+        'BAdbQh377J451TOypWJwbUoX4uCJghuPH6YkDaM6YQetgActTPr4rKK-xr6HhUnjmD2oId7KZ_56aRDcTz_mQlo',
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,8 +9,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
